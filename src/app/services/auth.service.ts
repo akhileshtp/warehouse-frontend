254e9f78eb10d0d292928dcd73b0929868b6947e
Akhilesh TP <panakhi@gmail.com>
2024-11-02 22:40:31 +0530

    'second'

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,24 +12,24 @@ export class AuthService {
   isLoggedIn$ = new BehaviorSubject<boolean>(false);
 
   registerService(registerObj: any){
-    return this.http.post<any>(`${this.url}register`, registerObj);
+    return this.http.post<any>(`${apiUrls.authServiceApi}register`, registerObj);
   }
 
   
   registerAdminService(registerObj: any) {
-    return this.http.post<any>(`${this.url}register-admin`, registerObj);
+    return this.http.post<any>(`${apiUrls.authServiceApi}register-admin`, registerObj);
   }
 
   loginService(loginObj: any){
-    return this.http.post<any>(`${this.url}login`, loginObj, {withCredentials: true});
+    return this.http.post<any>(`${apiUrls.authServiceApi}login`, loginObj, {withCredentials: true});
   }
 
   sendEmailService(email: string){
-    return this.http.post<any>(`${this.url}send-email`, {email: email});
+    return this.http.post<any>(`${apiUrls.authServiceApi}send-email`, {email: email});
   }
 
   resetPasswordService(resetObj: any) {
-    return this.http.post<any>(`${this.url}reset-password`, resetObj);
+    return this.http.post<any>(`${apiUrls.authServiceApi}reset-password`, resetObj);
   }
 
   isLoggedIn(){
