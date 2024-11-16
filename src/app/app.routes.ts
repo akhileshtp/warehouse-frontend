a218c2874d02d0d731d537b7c1840ca65480f150
akhileshtp <panakhi@gmail.com>
2024-11-16 17:59:52 +0530

    footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LaborerViewComponent } from './pages/laborer-view/laborer-view.componen
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
-  //{ path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path:'login', loadComponent: ()=> import('./pages/login/login.component')},
   { path: 'register', loadComponent: () => import('./pages/register/register.component') },
   { path: 'home', loadComponent: () => import('./pages/home/home.component') },
